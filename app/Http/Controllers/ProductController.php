e9a878081f8d2a91ea955e04a3c182c344edefe9
SegunCodes <shegstix64@gmail.com>
2022-12-01 23:52:03 +0100

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+// use Image;
+
+class ProductController extends Controller
+{
+    public function getAllProducts(Request $request){
+        $products = Product::all();
+        return response()->json([
+            'products' => $products
+        ], 200);
+    }
+
+    public function addProduct(Request $request){
+        $product = new Product();
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $image = $request->photo;  // base64 encoded image
+        $image = str_replace('data:image/jpeg;base64,', '', $image);
+        $image = str_replace(' ', '+', $image);
+        $imageName = Str::random(10).'.'.'jpg';
+        \File::put(public_path('upload'). '/' . $imageName, base64_decode($image));
+        $product->photo = $imageName;
+        $product->type = $request->type;
+        $product->quantity = $request->quantity;
+        $product->price = $request->price;
+        $product->save();
+    }
+}
---
+++ resources/js/router/index.js
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import productIndex from '../components/products/index.vue'
+import productNew from '../components/products/new.vue'
+import notFound from '../components/notFound.vue'
+
+const routes = [
+    {
+        path: '/',
+        component: productIndex
+    },
+    {
+        path: '/product/new',
+        component: productNew
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        component: notFound
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+})
+export default router
